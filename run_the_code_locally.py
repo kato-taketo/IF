f0abc8486e6319ac00855e0e497f6561b08e624f
tkato10315 <take10tokyohkit@gmail.com>
2023-06-27 17:46:51 +0900

    change cuda devices

--- run_the_code_locally.py
+++ run_the_code_locally.py
@@ -1,7 +1,7 @@
 from deepfloyd_if.modules import IFStageI, IFStageII, StableStageIII
 from deepfloyd_if.modules.t5 import T5Embedder
 
-device = 'cuda:0'
+device = 'cuda:0,1,2'
 if_I = IFStageI('IF-I-XL-v1.0', device=device)
 if_II = IFStageII('IF-II-L-v1.0', device=device)
 if_III = StableStageIII('stable-diffusion-x4-upscaler', device=device)
